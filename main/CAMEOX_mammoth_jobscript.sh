bd7d6e8163d867eb58fda26b597370a5afbf763a
khyox <jse.mnl@gmail.com>
2021-08-04 00:22:47 -0700

    Add files relevant to the batch system
    On branch main
    	new file:   main/CAMEOX_mammoth_jobscript.sh
    	new file:   main/aroB_infA_ecoli_GREMLIN_debug.txt
    	new file:   main/aroB_infA_ecoli_GREMLIN_pbatch.txt
    	new file:   main/aroB_infA_ecoli_uref100_debug.txt
    	new file:   main/aroB_infA_ecoli_uref100_pbatch.txt
    	new file:   main/aroB_infA_pf5_uref100_debug.txt
    	new file:   main/aroB_infA_pf5_uref100_pbatch.txt

---
+++ main/CAMEOX_mammoth_jobscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -M mammoth
+#SBATCH -p pbatch
+#SBATCH -t 1-00:00:00
+#SBATCH -J CAMEOX
+#SBATCH -N 1
+
+# USAGE:
+# sbatch CAMEOX_mammoth_jobscript.sh <CAMEOX_params_file>
+
+export OMP_NUM_THREADS=128
+cd /g/g92/metagen/cameos/CAMEOX/main/
+echo 'CAMEOX starts...'
+/usr/gapps/julia/bin/julia-1.6.2 -t 128 main.jl $1
+echo 'CAMEOX run DONE!'
